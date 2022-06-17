7735fdb0bfdb8227c09371fcc7abb502b26a960c
AndradeRevillaErick <43711671+AndradeRevillaErick@users.noreply.github.com>
2022-06-16 22:12:53 -0500

    docs:add description to mutations querys and fields (#7)

--- src/core/infrastructure/responses/paginate.response.ts
+++ src/core/infrastructure/responses/paginate.response.ts
@@ -3,11 +3,11 @@ import { ArgsType, ClassType, Field, Int, ObjectType } from "type-graphql";
 
 @ArgsType()
 export class PaginateArgs {
-  @Field(() => Int)
+  @Field(() => Int, { description: "Selected page." })
   @Min(0)
   public page = 1;
 
-  @Field(() => Int)
+  @Field(() => Int, { description: "Records limit per page." })
   @Min(5)
   @Max(50)
   public limit = 10;
@@ -15,23 +15,23 @@ export class PaginateArgs {
 
 @ObjectType()
 export class PaginateInfo {
-  @Field(() => Int)
+  @Field(() => Int, { description: "Current page." })
   public page!: number;
 
-  @Field(() => Int)
+  @Field(() => Int, { description: "Number of pages." })
   public pages!: number;
 
-  @Field(() => Int)
+  @Field(() => Int, { description: "Total number of records on page." })
   public total!: number;
 }
 
 export function PaginateResponse<TModel>(TModelClass: ClassType<TModel>) {
   @ObjectType({ isAbstract: true })
   abstract class PaginatedResponseClass {
-    @Field(() => PaginateInfo)
+    @Field(() => PaginateInfo, { description: "Pagination information." })
     public info!: PaginateInfo;
 
-    @Field(() => [TModelClass])
+    @Field(() => [TModelClass], { description: "Requested information." })
     public results!: TModel[];
   }
   return PaginatedResponseClass;
