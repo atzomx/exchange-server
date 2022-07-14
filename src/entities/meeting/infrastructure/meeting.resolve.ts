8c562dadc3beb2e24a14b15b536374e56e7bac9c
joseriosjose <37422662+joseriosjose@users.noreply.github.com>
2022-07-13 22:29:01 -0500

    feat(entitie): add meeting entity (#15)

---
+++ src/entities/meeting/infrastructure/meeting.resolve.ts
@@ -0,0 +1,56 @@
+import {
+  ValidateArgs,
+  ValidateIdentifier,
+} from "@core/infrastructure/decorators";
+import { Arg, Args, Mutation, Query, Resolver } from "type-graphql";
+import MeetingController from "../application/meeting.controller";
+import Meeting from "../domain/meeting.entity";
+import { MeetingPaginateArgs } from "./meeting.args";
+import { MeetingInputCreate, MeetingInputUpdate } from "./meeting.inputs";
+import { MeetingPaginateResponse } from "./meeting.response";
+
+@Resolver(Meeting)
+class MeetingResolver {
+  private controller: MeetingController;
+
+  constructor() {
+    this.controller = new MeetingController();
+  }
+
+  @Query(() => Meeting, { description: "Returns one meeting by id" })
+  async meetingById(@Arg("id") id: string): Promise<Meeting> {
+    const user = await this.controller.meetingById(id);
+    return user;
+  }
+
+  @Query(() => MeetingPaginateResponse, {
+    description: "Returns an array of direction",
+  })
+  async meetingPaginate(@Args() data: MeetingPaginateArgs) {
+    const results = await this.controller.meetingPaginate(data);
+    return results;
+  }
+
+  @Mutation(() => Meeting)
+  @ValidateArgs(MeetingInputCreate, "data")
+  async meetingCreate(@Arg("data") direction: MeetingInputCreate) {
+    const result = await this.controller.meetingCreate(direction);
+    return result;
+  }
+
+  @Mutation(() => Meeting)
+  @ValidateIdentifier(MeetingInputUpdate, "id")
+  @ValidateArgs(MeetingInputUpdate, "data")
+  async meetingUpdate(
+    @Arg("id") id: string,
+    @Arg("data") direction: MeetingInputUpdate,
+  ) {
+    const result = await this.controller.meetingUpdate(
+      id.toString(),
+      direction,
+    );
+    return result;
+  }
+}
+
+export default MeetingResolver;
