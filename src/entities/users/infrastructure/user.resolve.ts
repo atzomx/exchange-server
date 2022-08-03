1130b02b226e4c270a33de006d21d444d0c4db3f
joseriosjose <37422662+joseriosjose@users.noreply.github.com>
2022-08-02 21:41:36 -0500

    feat(bot): add autoassign bot (#29)

--- src/entities/users/infrastructure/user.resolve.ts
+++ src/entities/users/infrastructure/user.resolve.ts
@@ -53,7 +53,6 @@ class UserResolver {
   @ValidateArgs(UserInputCreate, "data")
   async create(@Arg("data") user: UserInputCreate) {
     const result = await this.controller.create(user);
-    console.log("algo");
     return result;
   }
 
